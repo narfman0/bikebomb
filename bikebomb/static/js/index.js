d30e2bd98247b1b2443d2cdd2cf9fd8d56a9c1ce
Jon Robison <narfman0@gmail.com>
2016-03-10 11:24:48 -0500

    -Refactor to incorporate pagination and some performance related enhancements

--- bikebomb/static/js/index.js
+++ bikebomb/static/js/index.js
@@ -1,23 +1,47 @@
 var BootstrapTable = ReactBootstrapTable.BootstrapTable;
 var TableHeaderColumn = ReactBootstrapTable.TableHeaderColumn;
-
-$.getJSON( "/api/models/", function(data) {
-  ReactDOM.render(
-    <BootstrapTable data={data.results} pagination={true}>
-        <TableHeaderColumn dataField="id" isKey={true}>Product ID</TableHeaderColumn>
-        <TableHeaderColumn
-          dataField="make" filter={{type: "TextFilter", placeholder: "Please enter a value"}}>
-          Make
-        </TableHeaderColumn>
-        <TableHeaderColumn
-          dataField="name" filter={{type: "TextFilter", placeholder: "Please enter a value"}}>
-          Model
-        </TableHeaderColumn>
-        <TableHeaderColumn
-          dataField="years" filter={{type: "TextFilter", placeholder: "Please enter a value"}}>
-          Years
-        </TableHeaderColumn>
-    </BootstrapTable>,
+var BikeTable = React.createClass({
+  loadNextModelPage: function() {
+    var modelPage = this.state.modelPage;
+    var models = this.state.models;
+    var url = this.props.url + '?page=' + modelPage;
+    $.getJSON(url, function(data) {
+      models = models.concat(data.results);
+      this.setState({models: models, modelPage: modelPage + 1});
+      if(data.next){
+        setTimeout(function(){
+          this.loadNextModelPage();
+        }.bind(this), 0);
+      }
+    }.bind(this));
+  },
+  componentDidMount: function() {
+    this.loadNextModelPage();
+  },
+  getInitialState: function() {
+    return {models: [], modelPage: 1};
+  },
+  render: function() {
+    return (
+      <BootstrapTable data={this.state.models} pagination={true} striped={true} hover={true} condensed={true}>
+          <TableHeaderColumn dataField="id" isKey={true}>Product ID</TableHeaderColumn>
+          <TableHeaderColumn
+            dataField="make" filter={{type: "TextFilter", placeholder: "Please enter a value"}}>
+            Make
+          </TableHeaderColumn>
+          <TableHeaderColumn
+            dataField="name" filter={{type: "TextFilter", placeholder: "Please enter a value"}}>
+            Model
+          </TableHeaderColumn>
+          <TableHeaderColumn
+            dataField="years" filter={{type: "TextFilter", placeholder: "Please enter a value"}}>
+            Years
+          </TableHeaderColumn>
+      </BootstrapTable>
+    );
+  }
+});
+ReactDOM.render(
+  <BikeTable url="/api/models/" />,
   document.getElementById("content")
-  );
-})
+);
