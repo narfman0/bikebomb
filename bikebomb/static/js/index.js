ccd5f09a737f1e1a3942fa8aa311078a8805f2b2
Jon Robison <narfman0@gmail.com>
2016-03-09 23:36:05 -0500

    -Add basic react example

---
+++ bikebomb/static/js/index.js
@@ -0,0 +1,40 @@
+var BootstrapTable = ReactBootstrapTable.BootstrapTable;
+var TableHeaderColumn = ReactBootstrapTable.TableHeaderColumn;
+
+// products will be presented by ReactBsTable
+var products = [
+  {
+      id: 1,
+      name: "Product1",
+      price: 120
+  },{
+      id: 2,
+      name: "Product2",
+      price: 80
+  },{
+      id: 3,
+      name: "Product3",
+      price: 207
+  },{
+      id: 4,
+      name: "Product4",
+      price: 100
+  },{
+      id: 5,
+      name: "Product5",
+      price: 150
+  },{
+      id: 6,
+      name: "Product1",
+      price: 160
+  }
+];
+
+ReactDOM.render(
+  <BootstrapTable data={products} striped={true} hover={true}>
+      <TableHeaderColumn isKey={true} dataField="id">Product ID</TableHeaderColumn>
+      <TableHeaderColumn dataField="name">Product Name</TableHeaderColumn>
+      <TableHeaderColumn dataField="price">Product Price</TableHeaderColumn>
+  </BootstrapTable>,
+    document.getElementById("content")
+);
